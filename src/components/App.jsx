70afa918fae184c3c0cf02d8fd02bf8190d7193c
Jayakrishna Surampudi <Jayakrishna.jk14@gmail.com>
2022-01-21 18:21:17 +0530

    ternary operator used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Form from "./Form";
-var userIsRegistered = false;
 
-function renderConditionally() {
-  if (userIsRegistered === true) {
-    return <h1>hello</h1>;
-  } else {
-    return <Form />;
-  }
-}
+var isLogged = true;
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return isLogged ? (
+    <h1>
+      used ternary operator for displaying function as an operator if user is
+      logged in{" "}
+    </h1>
+  ) : (
+    <Form />
+  );
 }
 
 export default App;
